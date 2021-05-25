8822df2910035fe14ead5ab02ac77a598d7a4790
Алексей <61280444+govorunovalex@users.noreply.github.com>
2021-05-25 22:14:01 +0300

    innit laba12

---
+++ text_editor_lib/TreeIterator.h
@@ -0,0 +1,17 @@
+#pragma once
+#include "Tree.h"
+
+class TTreeIterator {
+  protected:
+    TTree * root;
+    TTree *current;
+     TStackList < TTree * >stack;
+  public:
+     TTreeIterator(TTree * _root);
+    TTree *GoNext();
+    void Reset();
+    bool IsEnd();
+    void PutInStack(TTree * tree);
+    TTree *operator() ();
+     TTreeIterator & operator++();
+};
